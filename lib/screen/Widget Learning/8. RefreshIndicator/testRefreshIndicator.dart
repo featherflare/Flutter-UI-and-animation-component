a85ab28cb4a6f13ca25a09631b9f7c6c34f5a6be
dachatorn <dachatorn.a@kegroup.co.th>
2022-09-12 14:05:34 +0700

    Test Widget RotatedBox

---
+++ lib/screen/Widget Learning/8. RefreshIndicator/testRefreshIndicator.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TestRefreshIndicator extends StatefulWidget {
+  const TestRefreshIndicator({Key? key}) : super(key: key);
+
+  @override
+  State<TestRefreshIndicator> createState() => _TestRefreshIndicatorState();
+}
+
+class _TestRefreshIndicatorState extends State<TestRefreshIndicator> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RefreshIndicator Sample'),
+      ),
+      body: RefreshIndicator(
+        key: _refreshIndicatorKey,
+        triggerMode: RefreshIndicatorTriggerMode.onEdge,
+        displacement: 300, // margin top
+        edgeOffset: 20,
+        strokeWidth: 4,
+        color: Colors.white,
+        backgroundColor: Colors.blue,
+        child: Scrollbar(
+          isAlwaysShown: true,
+          showTrackOnHover: true,
+          child: ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text('Item $index'),
+              );
+            },
+          ),
+        ),
+        onRefresh: () async {
+          return Future<void>.delayed(const Duration(seconds: 3));
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Show refresh indicator programmatically on button tap.
+          _refreshIndicatorKey.currentState?.show();
+        },
+        icon: const Icon(Icons.refresh),
+        label: const Text('Show Indicator'),
+      ),
+    );
+  }
+}
